8d9281075b6f08d706a161ae1d2253d08233a8c2
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-12 18:39:03 +0530

    create post route for create contact list

--- index.js
+++ index.js
@@ -9,8 +9,24 @@ const Contact = require("./models/contact");
 
 // add middleware from body parser
 app.use(express.json());
-app.get("/", (req, resp) => {
-  resp.send("it is running");
+
+// post route for create contact
+app.post("/create_contact", function (req, resp) {
+  Contact.create({
+    name: req.body.name,
+    phone: req.body.phone,
+  }),
+    function (err, newContact) {
+      if (err) {
+        console.log("err in create contact list");
+        return;
+      }
+      return resp.json({
+        message: "Contact created",
+        data: newContact,
+      });
+    };
+  console.log(message);
 });
 
 app.listen(port, (err) => {
