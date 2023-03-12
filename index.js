9ee725c623ac6925f4471e72292ee195273172b7
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-12 18:20:57 +0530

    import database connection / and/ import contactSchema, // add middleware for body parser

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 8600;
+// database connection
+const db = require("./config/mongoose");
 
+// import contactSchema
+const Contact = require("./models/contact");
+
+// add middleware from body parser
+app.use(express.json());
 app.get("/", (req, resp) => {
   resp.send("it is running");
 });
